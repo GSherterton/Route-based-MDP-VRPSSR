43b4327a53196a2d31d87a1430d75ded6c309dcb
GSHerterton <gsherterton@gmail.com>
2024-09-11 13:57:25 -0300

    Colocando os sets responsaveis pela atualizacao do state

--- headers/State.h
+++ headers/State.h
@@ -13,7 +13,16 @@ class State{
         vector<int> status;
         vector<int> route_plan;
         
+        //idea
+        /*instead of using a vector to stores the costumers status we could have a vector
+        to store the status of each vertex of the original matrix*/
+        /*using this we could erase the information of a specific customer, and a costumer
+        now will be just a possible location, so we'll just search if a vertex has a requested*/
+        //the question is, this will descaracterize our model?
+        //this will save time but use more memory
+
     public:
+        State();
         State(const Config& config); //talvez não precise passar a config como parametro
 
         inline int get_decision_epoch() const { return decision_epoch; }
@@ -22,5 +31,10 @@ class State{
         inline vector<int> get_status() const { return status; }
         inline vector<int> get_route_plan() const { return route_plan; }
 
-        void set_requested(const int& request) { status[request] = 1; }
+        void advance_decision_epoch() { decision_epoch++; }
+        void update_position(const int& position) { this->position = position; }
+        void advance_time(const int& time) { this->time += time; } //adds a time to the actual time
+        void set_requested(const int& request) { status[request] = 1; } //set a request to a costumer, passing the costumer index
+        //void set_requested(const int& position) { status[position] = 1; } //set a request to a costumer, passing the costumer position
+        void update_route_plan(const vector<int>& route_plan) { this->route_plan = route_plan; }
 };
--- sources/State.cpp
+++ sources/State.cpp
@@ -1,10 +1,13 @@
 #include "State.h"
 
+State::State(){}
+
 //o estado inicial começa na decision_epoch 0, no depot, com o
 //tempo zerado e com os status todos zerados
 State::State(const Config& config){
     decision_epoch = 0;
     position = config.get_depot();
     time = 0;
-    status = vector(config.get_customers_size(), 0);
+    status = vector<int>(config.get_customers_size(), 0); //customer status
+    //status = vector<int>(config.get_dimension(), 0); //vertex status
 }
