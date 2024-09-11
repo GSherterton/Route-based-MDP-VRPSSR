8ee138314afc32498af424171f891a62f095ebc0
GSHerterton <gsherterton@gmail.com>
2024-09-11 13:56:51 -0300

    Adicionando a chegada dos requests com o passar do decision epoch

--- headers/Control.h
+++ headers/Control.h
@@ -7,17 +7,20 @@ using namespace std;
 
 class Control{
     private:
-        int decision_epoch_horizon;
         string instance_name;
+        int decision_epoch_horizon;
         Config config;
         State state;
 
         int request_index;
-        vector<pair<int, int>> requests;//costumer, time of the request
+        vector<pair<int, int>> requests;//costumer index, time of the request
+        //vector<pair<int, int>> requests;//costumer position, time of the request
 
         int total_reward;
 
+        void add_request(const int& index, const int& time);
         void receive_requests();
+        void action(const int& elapsed_time, const int& new_position);
     public:
         Control(const string& instance_name, const int& decision_epoch_horizon);
         void initiate();
--- sources/Control.cpp
+++ sources/Control.cpp
@@ -1,15 +1,42 @@
 #include "Control.h"
 
+void Control::add_request(const int& index, const int& time){
+    requests.push_back(make_pair(index, time));
+}
+
 Control::Control(const string& instance_name, const int& decision_epoch_horizon){
     this->instance_name = instance_name;
     this->decision_epoch_horizon = decision_epoch_horizon;
     
     request_index = 0;
+    total_reward = 0;
     
     //initialize the config
     config = Config(instance_name, decision_epoch_horizon);
     //initialize the state
     state = State(config);
+
+    //inicializar os requests manualmente com o index do customer
+    add_request(0, 0);
+    add_request(1, 0);
+    add_request(4, 1);
+    add_request(5, 2);
+    add_request(2, 2);
+    add_request(7, 4);
+    add_request(8, 8);
+    add_request(6, 11);
+    add_request(3, 20);
+
+    //inicializar os requests manualmente com a posicao do customer
+    /*add_request(costumer[0], 0);
+    add_request(costumer[1], 0);
+    add_request(costumer[4], 1);
+    add_request(costumer[5], 2);
+    add_request(costumer[2], 2);
+    add_request(costumer[7], 4);
+    add_request(costumer[8], 8);
+    add_request(costumer[6], 11);
+    add_request(costumer[3], 20);*/
 }
 
 void Control::receive_requests(){
@@ -25,11 +52,34 @@ void Control::receive_requests(){
     }
 }
 
+void Control::action(const int& elapsed_time, const int& new_position){
+    state.advance_decision_epoch();
+    state.update_position(state.get_position());//the position remains the same
+    state.advance_time(elapsed_time);
+    //no set request
+    total_reward += 0;//has to update the total reward
+}
+
 void Control::initiate(){
     //iterates above the decision epoch horizon
     for(int i = 0; i < decision_epoch_horizon; i++){
         receive_requests(); //receive the requests
+
+        vector<int> actual_status = state.get_status();//pega os status atuais
+
+        cout << "Actual Requests: [";//debug if the requests are being made right
+        for(int j = 0; j < config.get_customers_size(); j++){
+            if(actual_status[j] == 1){
+                cout << j << " ";
+            }
+        }cout << "]" << endl;
+
         //take an action
+        //for now will be do nothing just pass the time
+        int elapsed_time = 2;
+        int new_position = 0;
+        action(elapsed_time, new_position);
+
         //do the transition
     }
 }
