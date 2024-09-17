9ee6442f188e460c2f9b26f3346b8d8a73f32bd6
GSHerterton <gsherterton@gmail.com>
2024-09-17 18:37:09 -0300

    Main com o dijkstra e o A* para comparar as rotas geradas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <queue>
+#include <set>
 #include "Control.h"
 #include "Dijkstra.h"
 #include "A_star.h"
@@ -78,6 +80,36 @@ int main(int argc, char** argv){
     //  {15, 16, 17, 18, 19},
     //  {20, 21, 22, 23, 24}}
 
+    /*//testing the priority queue
+    priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> queue;
+    queue.push(make_pair(1, 1));
+    queue.push(make_pair(1, 2));
+    queue.push(make_pair(1, 1));
+    queue.push(make_pair(1, 1));
+    queue.push(make_pair(1, 3));
+    queue.push(make_pair(1, 9));
+
+    int n = queue.size();
+    for(int i = 0; i < n; i++){
+        cout << queue.top().first << " " << queue.top().second << endl;
+        queue.pop();
+    }
+    */
+
+    /*//testing the set
+    set<pair<double, int>> set;
+    set.insert(make_pair(1, 1));
+    set.insert(make_pair(1, 2));
+    set.insert(make_pair(2, 1));
+    set.insert(make_pair(3, 1));
+    set.insert(make_pair(6, 3));
+    set.insert(make_pair(3, 9));
+
+    for(auto it : set){
+        cout << it.first << " " << it.second << endl;
+    }
+    */
+
     if(argc < 2){//at least the instance name is needed
         cout << "Missing parameters!\n";
 
@@ -89,22 +121,26 @@ int main(int argc, char** argv){
     vector<double> x;
     vector<double> y;
     vector<vector<double>> distance_matrix = read_instance(instance_name, x, y);
+    Route route;
 
-    int start, end;
-    cin >> start >> end;
+    while(1){
+        int start, end;
+        cin >> start >> end;
 
-    /*//testing the dijkstra
-    Dijkstra dijkstra(distance_matrix);
-    Route route = dijkstra.shortest_path(start, end); //runs dijsktra passing the start and the end vertex
-    */
-    
-    //warning: i changed the instance articleA to have the position of each vertex
-    //testing the A*
-    A_star a_star(distance_matrix, x, y);
-    Route route = a_star.shortest_path(start, end); //runs a_star passing the start and the end vertex
-    
+        //testing the dijkstra
+        Dijkstra dijkstra(distance_matrix);
+        route = dijkstra.shortest_path(start, end); //runs dijsktra passing the start and the end vertex
+        
+        route.print_route();
 
-    route.print_route();
+        //warning: i changed the instance articleA to have the position of each vertex
+        //testing the A*
+        A_star a_star(distance_matrix, x, y);
+        route = a_star.shortest_path(start, end); //runs a_star passing the start and the end vertex
+        
+
+        route.print_route();
+    }
 
     /*
     //initialize the control passing the instance name and with decision_epoch_horizon
